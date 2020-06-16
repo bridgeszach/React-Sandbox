2a1661ae158921eefefc94a8c55d86f576e86bd6
Zach Bridges <56745560+bridgeszach@users.noreply.github.com>
2020-06-16 16:23:59 -0400

    2nd Commit - Incrementing

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counterComponent'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Counter />
+ReactDOM.render( 
+  <React.StrictMode> 
+  <
+  Counter / >
   </React.StrictMode>,
   document.getElementById('root')
-);
+); 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
